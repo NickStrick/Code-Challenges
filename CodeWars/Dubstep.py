b649d4976478830d37662cbb5d1603f0453082e2
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-11-13 11:46:11 -0600

    dubstep codewars

---
+++ CodeWars/Dubstep.py
@@ -0,0 +1,15 @@
+def song_decoder(song):
+    song = song.split("WUB")
+
+    while("" in song):
+        song.remove("")
+    song = " ".join(song)
+
+    return song
+
+
+print(song_decoder("AWUBBWUBC"), "A B C", "WUB should be replaced by 1 space")
+print(song_decoder("AWUBWUBWUBBWUBWUBWUBC"), "A B C",
+      "multiples WUB should be replaced by only 1 space")
+print(song_decoder("WUBAWUBBWUBCWUB"), "A B C",
+      "heading or trailing spaces should be removed")
