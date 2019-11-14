39db04b58088b52ca7dbdfceb437ebcc5d76f006
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-11-14 14:59:11 -0600

    added group code

--- LeetCode/MaxLevelSum.js
+++ LeetCode/MaxLevelSum.js
@@ -47,3 +47,29 @@ var maxLevelSum = function (root) {
     return maxLvl
 
 };
+
+// Solution 2
+//     if(!root) return;
+
+//     let values = [];
+//     let max = -Infinity, output = 0;
+
+//     function traverse(node, level){
+//         if (!values[level]) values.push(node.val);
+//         else values[level] += node.val;
+
+//         if (node.left) traverse(node.left, level + 1);
+//         if (node.right) traverse(node.right, level + 1);
+//     }
+
+//     traverse(root, 0);
+
+//     values.forEach(value, i => {
+//         if (value > max){
+//             max = value;
+//             output = i;
+//         }
+//     })
+
+//     return output + 1;
+// };
