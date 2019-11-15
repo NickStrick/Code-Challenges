907fa0d13b5aba153570a03fc431454eef8ca237
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-11-15 16:32:45 -0600

    added encryption problem

---
+++ HackerRank/Encryption.js
@@ -0,0 +1,19 @@
+//https://www.hackerrank.com/challenges/encryption/problem
+
+function encryption(s) {
+    let result = '';
+    let length = s.length;
+
+    let cols = Math.ceil(Math.sqrt(length));
+
+    for (let i = 0; i < cols; i++) {
+
+        for (let j = i; j < length; j += cols) {
+            result += s[j];
+        }
+        result += ' ';
+    }
+
+    return result;
+
+}
