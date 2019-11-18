46f4ee53e579b59c1f73e69872635e0e9b767077
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-11-18 16:49:46 -0600

    codewars sort the odd

--- CodeWars/sortTheOdd.py
+++ CodeWars/sortTheOdd.py
@@ -14,6 +14,10 @@ def sort_array(source_array):
     return source_array
     # Return a sorted array.
 
+# def sort_array(arr):
+#   odds = sorted((x for x in arr if x%2 != 0), reverse=True)
+#   return [x if x%2==0 else odds.pop() for x in arr]
+
 
 print(sort_array([5, 3, 2, 8, 1, 4]), [1, 3, 2, 8, 5, 4])
 print(sort_array([5, 3, 1, 8, 0]), [1, 3, 5, 8, 0])
