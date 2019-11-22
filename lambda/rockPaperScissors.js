26d965e237d99f4c8ee625fab27ef26a1b392ee2
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-11-21 18:43:55 -0600

    updated rockpaperscissors challegen

--- lambda/rockPaperScissors.js
+++ lambda/rockPaperScissors.js
@@ -1,1 +1,25 @@
-function rockPaperScissors(n) { }
+function rockPaperScissors(n) {
+    const results = [];
+    const options = ['rock', 'paper', 'scissors'];
+
+    const pointers = Array(n).fill(0)
+    let pointer = pointers.length - 1
+
+    while (pointers[0] !== 3) {
+        while (pointers[pointer] < 3) {
+            console.log(pointers)
+            results.push(pointers.map((i) => options[i]))
+            pointers[pointer]++
+        }
+
+        do {
+            pointers[pointer] = 0
+            pointer--
+        } while (pointers[pointer] === 2 && pointer > 0)
+
+        pointers[pointer]++;
+        pointer = pointers.length - 1;
+    }
+
+    return results;
+}
