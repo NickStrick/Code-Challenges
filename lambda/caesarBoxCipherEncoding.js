9c44b28eca1208cca1a3d0b971f2b63fbc669fc7
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-12-04 16:00:19 -0600

    added problem description

--- lambda/caesarBoxCipherEncoding.js
+++ lambda/caesarBoxCipherEncoding.js
@@ -1,3 +1,32 @@
+/*
+Caesar Box
+Caesar Box is a simple transposition cipher used in the Roman Empire, in which characters of the given message are broken into multiple lines that form a square when stacked up and then read the square column by column.
+Given a message, encode it.
+Example
+For inputString = "a message", the output should be
+caesarBoxCipherEncoding(inputString) = "aea sgmse".
+The square will look as follows:
+a m
+ess
+age
+Thus, the encoded message will be aea sgmse.
+Input/Output
+[input] string inputString
+A string of length n2 for some integer n.
+Guaranteed constraints:
+1 ≤ inputString.length ≤ 16.
+[output] string
+  
+Test Cases:
+1:
+Input: inputString: "a message"
+Expected Output: "aea sgmse"
+2:
+Input: inputString: "sixteenletters16"
+Expected Output: "seerietsxnt1tle6"
+*/
+
+
 function caesarBoxCipherEncoding(inputString) {
 
     // square root of length
