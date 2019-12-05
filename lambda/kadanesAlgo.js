f115a80e11d052734003127d2f3eb03f38ecc56f
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-12-05 17:35:55 -0600

    whiteboarding problem

---
+++ lambda/kadanesAlgo.js
@@ -0,0 +1,17 @@
+//Write a function that takes in a non-empty array of integers and returns the maximum sum that can be obtained by summing up all the numbers in a non-empty subarray of the input array. A subarray must only contain adjacent numbers.
+let intArray = [3, 5, -9, 1, 3, -2, 3, 4, 7, 2, -9, 6, 3, 1, -5, 4]
+// 19
+function kadanesAlgo(array) {
+    let results = [];
+    for (let i = 0; i < array.length; i++) {
+        let temp = array[i];
+        results.push(temp)
+        for (j = i + 1; j < array.length; j++) {
+            results.push(array[j] + temp);
+            temp += array[j]
+        }
+    }
+    //console.log(results)
+    return Math.max(...results)
+}
+console.log(kadanesAlgo(intArray))
