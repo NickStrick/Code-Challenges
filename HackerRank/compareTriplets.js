57b8a1d6f050999168aa2a42f43711bb36833ea2
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-12-13 17:04:30 -0600

    added solution to triplets

--- HackerRank/compareTriplets.js
+++ HackerRank/compareTriplets.js
@@ -1,1 +1,39 @@
 //https://www.hackerrank.com/challenges/compare-the-triplets/problem?h_r=next-challenge&h_v=zen
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function (inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function () {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the compareTriplets function below.
+function compareTriplets(a, b) {
+    let res = [0, 0]
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] > b[i]) {
+            res[0] += 1
+        } else if (a[i] < b[i]) {
+            res[1] += 1
+        }
+    }
+    return res
+
+}
