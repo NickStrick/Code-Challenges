af71e5756282f08b9cb4e96c6ed178031216cd43
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-12-13 16:49:29 -0600

    completed sum challenge

--- HackerRank/SimpleArraySum.js
+++ HackerRank/SimpleArraySum.js
@@ -1,1 +1,39 @@
 //https://www.hackerrank.com/challenges/simple-array-sum/problem
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(str => str.trim());
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the simpleArraySum function below.
+ */
+function simpleArraySum(ar) {
+    /*
+     * Write your code here.
+     */
+    let sum = 0
+    for (let i = 0; i < ar.length; i++) {
+        sum += ar[i]
+    }
+    return sum;
+
+}
