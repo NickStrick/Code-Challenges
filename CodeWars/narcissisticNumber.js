98778b875996088e3a1b9f30a6dd803af0f54735
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-12-16 15:23:20 -0600

    added solution to narccistic

--- CodeWars/narcissisticNumber.js
+++ CodeWars/narcissisticNumber.js
@@ -1,1 +1,11 @@
 //https://www.codewars.com/kata/5287e858c6b5a9678200083c/train/javascript
+
+//solution 1
+function narcissistic(value) {
+    let arr = value.toString()
+    arr = arr.split('')
+    let pow = arr.length
+    let total = 0
+    arr.forEach(num => total += Math.pow(num, pow))
+    return total === value
+}
