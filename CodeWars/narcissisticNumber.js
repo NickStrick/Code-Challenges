2f0fef9bdf34bfe8ab996021e1e659ff90a3194d
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-12-16 18:30:47 -0600

    added second solution to narssictic

--- CodeWars/narcissisticNumber.js
+++ CodeWars/narcissisticNumber.js
@@ -9,3 +9,15 @@ function narcissistic(value) {
     arr.forEach(num => total += Math.pow(num, pow))
     return total === value
 }
+
+// function narcissistic(value) {
+//     // Code me to return true or false
+//     let strNum = value + '';
+//     let sum = 0;
+//     let exponent = strNum.length;
+//     for(let i = 0; i < strNum.length; i++) {
+//       sum += Math.pow(parseInt(strNum[i]), exponent);
+//       console.log(sum)
+//     }
+//     return sum === value
+//   }
