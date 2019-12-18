dc161dde8bc70b052f6f82ab18f47f2678c87cf2
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-12-17 21:54:34 -0600

    added solution

--- CodeWars/narccissticvalue.js
+++ CodeWars/narccissticvalue.js
@@ -1,4 +1,12 @@
 // narssisti c function
 function narcissistic(value) {
-
+    // Code me to return true or false
+    let strNum = value + '';
+    let sum = 0;
+    let exponent = strNum.length;
+    for (let i = 0; i < strNum.length; i++) {
+        sum += Math.pow(parseInt(strNum[i]), exponent);
+        console.log(sum)
+    }
+    return sum === value
 }
