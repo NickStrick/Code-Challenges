56a97ffa55f16889f91a10a924028a8eaeae7ed6
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-12-19 11:10:50 -0600

    added charles solution

--- CodeWars/stringRearangement.py
+++ CodeWars/stringRearangement.py
@@ -1,1 +1,26 @@
 # https://www.codewars.com/kata/simple-fun-number-73-strings-rearrangement/train/javascript
+
+
+from itertools import permutations
+
+
+def stringsRearrangement(inputArray):
+    def check(a, b):
+        c = 0
+        for i in range(len(a)):
+            if a[i] != b[i]:
+                c += 1
+            if c > 1:
+                return False
+        if c == 0:
+            return False
+        return True
+    for perm in permutations(inputArray):
+        found = True
+        for j in range(1, len(perm)):
+            if not check(perm[j-1], perm[j]):
+                found = False
+                break
+        if found:
+            return True
+    return False
