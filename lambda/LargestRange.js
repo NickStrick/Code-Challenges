403b9ee9cea487084d8dba4e371688fac1becf5d
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-12-19 14:34:23 -0600

    whiteboarding problem added

--- lambda/LargestRange.js
+++ lambda/LargestRange.js
@@ -1,8 +1,9 @@
 // [2,3,4,5,6] returns [2,6]
 // return largest range of numbers in arrray
 
-let array = [1, 11, 3, 0, 15, 5, 2, 4, 10, 7, 12, 6];
-//ouput [0,7]
+let array = [1, 11, 3, 0, 15, 5, 2, 4, 10, 7, 12, 6];//ouput [0,7]
+let array2 = [19, -1, 18, 17, 2, 10, 3, 12, 5, 16, 4, 11, 8, 7, 6, 15, 12, 12, 2, 1, 6, 13, 14] // output [10,19]
+
 
 let largestRange = array => {
     let bestRange = [];
@@ -36,3 +37,4 @@ let largestRange = array => {
 }
 
 console.log(largestRange(array));
+console.log(largestRange(array2));
