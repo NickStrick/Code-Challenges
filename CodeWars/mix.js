593538337ce544bde1ace33377f850add2517b20
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-12-20 16:55:17 -0600

    mix challenge

---
+++ CodeWars/mix.js
@@ -0,0 +1,4 @@
+  //create an object for each string to tally the chars
+  //keep track of length so you can determine longest string
+  //populate object with the value being the count and the key being the char
+  //have a check in case the string of i is lowercase then move forward
