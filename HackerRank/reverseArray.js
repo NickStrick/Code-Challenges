c74b5f949c47e8cd11f01204cd9d233315bf19d3
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-12-20 11:49:27 -0600

    added reverseArray solution

--- HackerRank/reverseArray.js
+++ HackerRank/reverseArray.js
@@ -5,3 +5,56 @@ In an array, , of size , each memory location has some unique index,  (where ), 
 Given an array, , of  integers, print each element in reverse order as a single line of space-separated integers.
 */
 
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function () {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the reverseArray function below.
+function reverseArray(a) {
+    let left = 0
+
+    for (let i = a.length - 1; i >= (Math.floor(a.length / 2)); i--) {
+        let temp = a[i];
+        a[i] = a[left];
+        a[left] = temp;
+        left += 1
+    }
+    return a
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const arrCount = parseInt(readLine(), 10);
+
+    const arr = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const res = reverseArray(arr);
+
+    ws.write(res.join(' ') + '\n');
+
+    ws.end();
+}
+
