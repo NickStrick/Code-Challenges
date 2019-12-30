966ea39eab03a2984d7d4002fff42b57349ff05d
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-12-30 17:30:46 -0600

    added sparcearray file

---
+++ HackerRank/SparceArray.js
@@ -0,0 +1,22 @@
+//sparce arrays
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
