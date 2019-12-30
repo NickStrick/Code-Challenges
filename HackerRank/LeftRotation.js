e8139b7414b150c6f1799adf8f64e325b0d45251
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-12-30 17:10:31 -0600

    added code for leftrotation

--- HackerRank/LeftRotation.js
+++ HackerRank/LeftRotation.js
@@ -1,1 +1,36 @@
 // Left Rotation
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine(nums, d) {
+
+}
+
+
+
+function main() {
+    const nd = inputString[0].split(' ');
+
+    const n = parseInt(nd[0], 10);
+
+    const d = parseInt(nd[1], 10);
+
+    const a = readLine(inputString[1].split(' '), d).split(' ').map(aTemp => parseInt(aTemp, 10));
+}
