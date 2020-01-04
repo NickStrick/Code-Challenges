64faae4373a40ab7eb0646143b9e4acadb821b40
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-01-04 13:00:09 -0600

    added solution to dynamic array

--- HackerRank/DynamicArray.js
+++ HackerRank/DynamicArray.js
@@ -1,1 +1,34 @@
 //Given N, Q, and Q queries, execute each query.
+
+function dynamicArray(n, queries) {
+
+    // Functions
+    const firstOperation = (query) => {
+        let s = ((query[1] ^ lastAnswer) % n)
+        if (!seq[s]) {
+            seq[s] = []
+        }
+        seq[s].push(query[2])
+    }
+    const secondOperation = (query) => {
+        let s = ((query[1] ^ lastAnswer) % n)
+        lastAnswer = seq[s][query[2]]
+
+        res.push(lastAnswer)
+    }
+
+    // Loop
+    let lastAnswer = 0;
+    let seq = {};
+    let res = []
+    queries.forEach((query) => {
+        if (query[0] === 1) {
+            firstOperation(query);
+        } else if (query[0] === 2) {
+            secondOperation(query);
+        }
+    })
+
+    return res
+
+}
