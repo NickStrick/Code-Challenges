6c1c5732cad408806bad1a8c5906696b776550b0
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-01-05 18:06:46 -0600

    added tests to dubstep

--- CodeWars/dubstep2.py
+++ CodeWars/dubstep2.py
@@ -7,3 +7,10 @@ def song_decoder(song):
     song = " ".join(song)
 
     return song
+
+
+print(song_decoder("AWUBBWUBC"), "A B C", "WUB should be replaced by 1 space")
+print(song_decoder("AWUBWUBWUBBWUBWUBWUBC"), "A B C",
+      "multiples WUB should be replaced by only 1 space")
+print(song_decoder("WUBAWUBBWUBCWUB"), "A B C",
+      "heading or trailing spaces should be removed")
