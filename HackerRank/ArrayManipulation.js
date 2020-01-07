6aff4e1e755fc16b992db9bd1e15122fceda3ef5
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-01-06 19:15:59 -0600

    added arrayManipulation

---
+++ HackerRank/ArrayManipulation.js
@@ -0,0 +1,4 @@
+//Complete the function arrayManipulation in the editor below. It must return an integer, the maximum value in the resulting array.
+// arrayManipulation has the following parameters:
+// n - the number of elements in your array
+// queries - a two dimensional array of queries where each queries[i] contains three integers, a, b, and k.
