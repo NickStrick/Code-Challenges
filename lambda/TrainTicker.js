e0d1bd674e20c45fe038d67d3873b9623c7b9c22
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-01-06 19:29:54 -0600

    added trainticker solution

--- lambda/TrainTicker.js
+++ lambda/TrainTicker.js
@@ -1,1 +1,15 @@
 //https://www.codewars.com/kata/ticker/train/javascript
+
+function display() {
+    let tick = 0
+    setInterval(() => {
+        console.log(ticker('Beautiful is better than ugly.', 10, tick))
+        tick++
+    }, 1000)
+}
+const ticker = (text, width, tick) => {
+    let textStr = text.padStart(width + text.length)
+    let sliceStr = textStr.slice(tick % textStr.length, width + (tick % textStr.length))
+    return sliceStr.padEnd(width)
+}
+display()
