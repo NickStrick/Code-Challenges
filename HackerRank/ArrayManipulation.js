80b999c8afe0b8695e327e1f9564924f6e4f23ef
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-01-06 19:16:46 -0600

    adde solution to arraymanipulation

--- HackerRank/ArrayManipulation.js
+++ HackerRank/ArrayManipulation.js
@@ -2,3 +2,24 @@
 // arrayManipulation has the following parameters:
 // n - the number of elements in your array
 // queries - a two dimensional array of queries where each queries[i] contains three integers, a, b, and k.
+
+// Complete the arrayManipulation function below.
+function arrayManipulation(n, queries) {
+
+    let res = new Array(n).fill(0);
+    let biggest = 0
+    console.log(n, queries, res)
+
+    for (let i = 0; i < queries.length; i++) {
+        let query = queries[i]
+        for (let j = query[0] - 1; j < query[1]; j++) {
+            res[j] += query[2]
+            if (res[j] > biggest) {
+                biggest = res[j]
+            }
+        }
+
+    }
+    return biggest
+
+}
