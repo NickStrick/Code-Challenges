c6a8f89f1244606becddf470cae92b2b2892fc86
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-01-08 15:25:02 -0600

    added predictthewinner function

--- LeetCode/predictTheWinner.js
+++ LeetCode/predictTheWinner.js
@@ -1,1 +1,17 @@
 //https://leetcode.com/problems/predict-the-winner/
+/*
+@param {number[]} nums
+@return {boolean}
+*/
+
+var PredictTheWinner = function (nums) {
+    //players can see at least the next number from either end
+    // player will pick the number farthest away from the biggest inner number
+    // if there are one or less inner numbers, player will pick the largest
+
+    //[5, 1, 5, 233, 7]
+
+
+}
+
+console.log(PredictTheWinner([5, 1, 5, 233, 7]))
