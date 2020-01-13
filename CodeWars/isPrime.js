47dd8bbcea4173885fd997bb072cdf1874899b08
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-01-13 15:40:32 -0600

    added prime solution

--- CodeWars/isPrime.js
+++ CodeWars/isPrime.js
@@ -1,1 +1,15 @@
 //https://www.codewars.com/kata/5262119038c0985a5b00029f/train/javascript
+
+
+function isPrime(num) {
+    if (num <= 2) {
+        return true
+    }
+    let sq = Math.ceil(Math.sqrt(num))
+    for (let i = 2; i <= sq; i++) {
+        if (num % i === 0) {
+            return false
+        }
+    }
+    return true
+}
