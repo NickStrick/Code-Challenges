215ca66bebbbcbf5b4223beed9502ff7f161d3df
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-01-21 15:26:15 -0600

    added spiralcopy

---
+++ lambda/spiralCopy.py
@@ -0,0 +1,9 @@
+# https://gist.github.com/seanchen1991/d067811ae85321d7f632d964ae9c5655
+
+
+def spiral_copy(matrix):
+  # be able to traverse along rows
+    # traverse along an array as per usual
+  # be able to traverse along columns
+    # iterate along each row
+      # print the element from that row corresponding to the column index
