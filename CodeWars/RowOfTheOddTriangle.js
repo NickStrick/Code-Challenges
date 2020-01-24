13bfe4505cf21b2309e09ca0c2e71873e3aac121
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-01-23 22:52:15 -0600

    added oddrow solution

--- CodeWars/RowOfTheOddTriangle.js
+++ CodeWars/RowOfTheOddTriangle.js
@@ -1,1 +1,18 @@
 // https://www.codewars.com/kata/5d5a7525207a674b71aa25b5/train/javascript
+
+function oddRow(n) {
+    let arr = []
+    if (n == 1) return [1]
+    if (n == 2) return [3, 5]
+    if (n.length % 2 == 1) {
+        arr.push((n * n) - 1)
+        arr.push((n * n) + 1)
+    } else {
+        arr.push(n * n)
+    }
+    while (arr.length < n) {
+        arr.unshift((arr[0]) - 2)
+        arr.push((arr[arr.length - 1]) + 2)
+    }
+    return arr
+}
