c606ce1703367fa6ec95a1b27844672271dd5e23
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-01-29 18:26:26 -0600

    2nd solution to anagrams

--- CodeWars/WhereMyAnagramsAt.js
+++ CodeWars/WhereMyAnagramsAt.js
@@ -1,32 +1,48 @@
 //https://www.codewars.com/kata/523a86aa4230ebb5420001e1/train/javascript
 
+// function anagrams(word, words) {
+
+//     const word1 = {}
+//     word.split("").forEach(item => {
+//         if (!word1[item]) {
+//             word1[item] = 0
+//         }
+//         word1[item] += 1
+//     })
+
+//     for (let i = 0; i < words.length; i++) {
+//         const currWord = words[i].split("");
+//         const word2 = {}
+//         for (let j = 0; j < currWord.length; j++) {
+//             let item = currWord[j];
+//             if (item in Object.keys(word1)) {
+
+//             }
+
+//             if (!word2[item]) {
+//                 word2[item] = 0
+//             }
+//             word2[item] += 1
+//         }
+//     }
+//     console.log(word1, word2)
+//     return [word1, word2]
+// }
+
 function anagrams(word, words) {
-
-    const word1 = {}
-    word.split("").forEach(item => {
-        if (!word1[item]) {
-            word1[item] = 0
+    word = word.split('').sort().join('').toLowerCase()
+    return words.filter((x) => {
+        if (x.length === word.length) {
+            const currentWord = x.split('').sort().join('').toLowerCase()
+            if (word === currentWord) {
+                return true
+            } else {
+                return false
+            }
+        } else {
+            return false
         }
-        word1[item] += 1
     })
-
-    for (let i = 0; i < words.length; i++) {
-        const currWord = words[i].split("");
-        const word2 = {}
-        for (let j = 0; j < currWord.length; j++) {
-            let item = currWord[j];
-            if (item in Object.keys(word1)) {
-
-            }
-
-            if (!word2[item]) {
-                word2[item] = 0
-            }
-            word2[item] += 1
-        }
-    }
-    console.log(word1, word2)
-    return [word1, word2]
 }
 
 console.log(anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']), ['aabb', 'bbaa']);
