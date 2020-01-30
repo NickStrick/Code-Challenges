1b1909e3009356a486ee75908cbc557b930295aa
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-01-29 18:29:39 -0600

    third solution added

--- CodeWars/WhereMyAnagramsAt.js
+++ CodeWars/WhereMyAnagramsAt.js
@@ -45,6 +45,29 @@ function anagrams(word, words) {
     })
 }
 
+
+
+// function anagrams(word, words) {
+//     return words.filter(currWord => isAnagram(word, currWord)); 
+//    }
+//    ​
+//    function isAnagram(str1, str2) {
+//      const s1Len = str1.length;
+
+//      if (s1Len !== str2.length) {
+//        return false;
+//      }
+//    ​
+//      const alphabet = Array(26).fill(0);
+//    ​
+//      for (let i = 0; i < s1Len; i++) {
+//        alphabet[str1.charCodeAt(i) - 97]++;
+//        alphabet[str2.charCodeAt(i) - 97]--;
+//      }
+//    ​
+//      return alphabet.every(letter => letter === 0);
+//    }
+
 // def anagrams(word, words):
 //     word_length = len(word)
 
