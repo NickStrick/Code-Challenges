42baf4f86978af9903c169bb44a4cfd46bea1e18
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-01-30 15:50:59 -0600

    squaresofarray solution 2

--- LeetCode/SquaresOfAnArray.js
+++ LeetCode/SquaresOfAnArray.js
@@ -11,3 +11,16 @@ var sortedSquares = function (A) {
     return A.sort(function (a, b) { return a - b })
 
 };
+
+//solution 2
+let sorted = function (A) {
+    const result = Array(A.length).fill(0);
+    let start = 0;
+    let end = A.length - 1;
+    let p = end;
+    while (start <= end) {
+        (A[start] ** 2 > A[end] ** 2 ? result[p] = A[start++] ** 2 : result[p] = A[end--] ** 2)
+        p--;
+    }
+    return result;
+};
