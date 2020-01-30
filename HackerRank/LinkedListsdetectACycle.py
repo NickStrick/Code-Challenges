b10dc40476eb65f1e06a1ed7055a5669a7d3732a
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-01-29 18:34:29 -0600

    linkedlistcycle js testing

--- HackerRank/LinkedListsdetectACycle.py
+++ HackerRank/LinkedListsdetectACycle.py
@@ -82,3 +82,13 @@ def has_cycle(head):
 #   return false;
 # }
 # ​
+# const n1 = new Node(1);
+# const n2 = new Node(2);
+# const n3 = new Node(3);
+# const n4 = new Node(4);
+# n1.next = n2;
+# n2.next = n3;
+# n3.next = n4;
+# n4.next = n2;
+# ​
+# console.log(hasCycle(n1));
