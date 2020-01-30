0a910a596a53efdeb2b6bd1c89e3c0d656e8b02a
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-01-29 18:32:00 -0600

    added linkedlist cycle file

---
+++ HackerRank/LinkedListsdetectACycle.py
@@ -0,0 +1,12 @@
+"""
+https://www.hackerrank.com/challenges/ctci-linked-list-cycle/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=linked-lists
+
+Detect a cycle in a linked list. Note that the head pointer may be 'None' if the list is empty.
+​
+A Node is defined as: 
+ 
+    class Node(object):
+        def __init__(self, data = None, next_node = None):
+            self.data = data
+            self.next = next_node
+"""
