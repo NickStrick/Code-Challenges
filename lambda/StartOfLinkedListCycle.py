2a9ee759874f653ed395a899229fdcf06ba77e0d
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-01-31 15:32:24 -0600

    linkedcycle tests added

--- lambda/StartOfLinkedListCycle.py
+++ lambda/StartOfLinkedListCycle.py
@@ -77,3 +77,22 @@ def find_start(head, cycle_length):
         ahead = ahead.next
 
     return behind
+
+
+if __name__ == '__main__':
+  head = Node(1)
+  head.next = Node(2)
+  head.next.next = Node(3)
+  head.next.next.next = Node(4)
+  head.next.next.next.next = Node(5)
+  head.next.next.next.next.next = Node(6)
+​
+  head.next.next.next.next.next.next = head.next.next
+  print("LinkedList cycle start: " + str(find_cycle_start(head).value))
+​
+  head.next.next.next.next.next.next = head.next.next.next
+  print("LinkedList cycle start: " + str(find_cycle_start(head).value))
+​
+  head.next.next.next.next.next.next = head
+ 
+  print("LinkedList cycle start: " + str(find_cycle_start(head).value))
