e2f369b3ad89bc814d1fa39586eeeb993baed218
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-02-03 21:38:59 -0600

    sum of intervals code

--- CodeWars/sumOfIntervals.js
+++ CodeWars/sumOfIntervals.js
@@ -1,1 +1,17 @@
 // https://www.codewars.com/kata/52b7ed099cdc285c300001cd/train/javascript
+
+function sumIntervals(intervals) {
+    //SET ARR TO COUNT SUM OF INTERVALS FROM
+    let arr = [];
+    //FOR EACH INTERVAL, PUSH NUMBERS FROM INDEX 0 TO INDEX 1
+    intervals.forEach((interval) => {
+        //START COUNTING FROM THE NEXT NUMBER TO INDEX 0 BY ADDING + 1 TO INDEX
+        for (let i = interval[0] + 1; i <= interval[1]; i++) {
+            arr.push(i);
+        }
+    });
+    //REMOVE DUPLICATES
+    arr = arr.filter((v, i, a) => a.indexOf(v) === i);
+    //COUNT
+    return arr.length;
+}
