00d21cb380b55ef3fe1cfe93c1dd35b65d79be71
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-02-04 17:55:53 -0600

    duplicatezero second solution

--- LeetCode/DuplicateZeros.py
+++ LeetCode/DuplicateZeros.py
@@ -14,3 +14,17 @@ class Solution:
                     arr[r] = arr[r - 1]
                 i += 1
             i += 1
+
+# class Solution:
+#     def duplicateZeros(self, arr: List[int]) -> None:
+#         """
+#         Do not return anything, modify arr in-place instead.
+#         """
+#         preSize=len(arr)
+#         i=0;
+#         while i<preSize:
+#             if arr[i]==0:
+#                 arr.insert(i,0)
+#                 i+=1
+#             i+=1
+#         del arr[preSize:]
