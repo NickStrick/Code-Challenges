79266a6240e1593eb8bbeb2f5d20ad11f1b39899
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-02-04 17:55:15 -0600

    duplicate solution added

--- LeetCode/DuplicateZeros.py
+++ LeetCode/DuplicateZeros.py
@@ -1,1 +1,16 @@
 # https://leetcode.com/problems/duplicate-zeros/
+
+
+class Solution:
+    def duplicateZeros(self, arr: List[int]) -> None:
+        """
+        Do not return anything, modify arr in-place instead.
+        """
+        lastIndex = len(arr) - 1
+        i = 0
+        while i < lastIndex:
+            if arr[i] == 0:
+                for r in range(lastIndex, i, -1):
+                    arr[r] = arr[r - 1]
+                i += 1
+            i += 1
