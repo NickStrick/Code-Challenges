944915a91e412ab65c2cc2f06aa32b3253a2a8a5
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-02-05 16:09:25 -0600

    earners solution added

--- sql/topEarners.js
+++ sql/topEarners.js
@@ -1,1 +1,9 @@
 // https://www.hackerrank.com/challenges/earnings-of-employees/
+/*
+
+SELECT salary * months AS earnings, COUNT(*) FROM employee
+GROUP BY earnings
+ORDER BY earnings DESC
+LIMIT 1
+
+*/
