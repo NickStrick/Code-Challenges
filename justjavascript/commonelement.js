40342dad9a54ac5470427c42715f69f18b0cb61d
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-02-10 22:09:32 -0600

    common element second soultion

--- justjavascript/commonelement.js
+++ justjavascript/commonelement.js
@@ -14,3 +14,15 @@ function sumOfDigits(num) {
     num = num.toString().split('')
     return num.reduce((acc, el) => acc + Number(el), 0);
 }
+
+function commonElements(arr1, arr2) {
+    let newArray = [];
+    for (let i = 0; i < arr1.length; i++) {
+        for (let j = 0; j < arr2.length; j++) {
+            if (arr1[i] === arr2[j]) {
+                newArray.push(arr1[i]);
+            }
+        }
+    }
+    return newArray;
+}
