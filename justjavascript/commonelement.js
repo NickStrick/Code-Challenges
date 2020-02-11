8f50d675ff3e01113d1911699a37023b0e85d1af
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-02-10 22:08:45 -0600

    common element solution

--- justjavascript/commonelement.js
+++ justjavascript/commonelement.js
@@ -1,1 +1,11 @@
 //https://justjavascript.com/
+
+function commonElements(arr1, arr2) {
+    let newArr = []
+    for (let ele of arr2) {
+        if (arr1.indexOf(ele) > -1) {
+            newArr.push(ele)
+        }
+    }
+    return newArr
+}
