148cf6a4aa6bf905409e682dfd166a59caaea4e5
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-02-11 19:26:38 -0600

    perfectsquares javascript solution

--- LeetCode/perfectSquares.js
+++ LeetCode/perfectSquares.js
@@ -17,3 +17,30 @@
 //             if n - (square1 + square2) in squares:
 //               return 3
 //         return 4
+
+var numSquares = n => {
+
+    //returns if the number x is a valid square root (  can be represented as the square of an integer )
+    let isSquare = x => Math.floor(Math.sqrt(x)) ** 2 === x
+
+    if (isSquare(n)) return 1 // that would be the fact that its equal to itself
+
+    // legendre's three square theorem: a natural number n can be represented as 
+    // the sum of three squares of integers if and only id : n!= 4^x ( 8*m+7 )
+    while (n % 4 === 0) n /= 4
+    if (n % 8 === 7) return 4 // if n%8!==7 that means that my number can be written
+    // as a sum of at most 3 squares
+    // , otherwise the answer is definitely 4 because of Lagrange's theorem.
+    // ok, we ruled out the possibility of result 4
+    // there are only 2 results remaining, 2 and 3
+
+    //manually checking for result 2
+    for (let i = 0; i <= n; i++) {
+        // if x=n-i*i   and x is a valid square then OBVIOUSLY
+        // n=i^2 +sqrt(x)^2 , so n is a square of two numbers
+        if (isSquare(n - i * i)) return 2
+    }
+
+    // otherwise
+    return 3
+}
