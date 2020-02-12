7e289c74f8c5f02b5b2b19805918333817ea5f3c
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-02-12 16:15:13 -0600

    15 days of learning file

---
+++ sql/15daysoflearning.js
@@ -0,0 +1,6 @@
+/*
+https://www.hackerrank.com/challenges/15-days-of-learning-sql/problem?h_r=next-challenge&h_v=zen
+
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
