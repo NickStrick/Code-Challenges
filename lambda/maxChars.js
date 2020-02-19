68bbc1a1aede14cc65853fffb03cab56628f6078
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-02-18 18:47:44 -0600

    maxChars solution

--- lambda/maxChars.js
+++ lambda/maxChars.js
@@ -1,1 +1,15 @@
 //  Object.keys(obj).includes(char) ? obj[char]++ : (obj[char] = 1);
+
+function maxChar(str) {
+    let obj = {}
+    for (let char of str) {
+        if (!obj[char]) {
+            obj[char] = 1
+        } else {
+            obj[char] += 1
+        }
+    }
+    return Object.keys(obj).reduce((a, b) => {
+        return obj[a] > obj[b] ? a : b
+    })
+}
