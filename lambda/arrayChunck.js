21ee161992fb4296ce48b49f23fb377358f864f9
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-02-18 18:45:20 -0600

    arraychunk secondsolution

--- lambda/arrayChunck.js
+++ lambda/arrayChunck.js
@@ -5,3 +5,17 @@ function chunk(array, size) {
     }
     return chunked
 }
+
+// second soolution
+function chunk(array, size) {
+    let chunked = []
+    for (let item of array) {
+        let lastItem = chunked[chunked.length - 1]
+        if (!lastItem || lastItem.length === size) {
+            chunked.push([item])
+        } else {
+            lastItem.push(item)
+        }
+    }
+    return chunked
+}
