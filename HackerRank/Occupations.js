a38425f2cb911949dd5f3f71fe7fbe76e82b616c
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-02-19 18:54:28 -0600

    occupations solution

--- HackerRank/Occupations.js
+++ HackerRank/Occupations.js
@@ -1,1 +1,15 @@
 //https://www.hackerrank.com/challenges/occupations/problem
+
+// SELECT 
+// MAX(Doctor)
+// , MAX(Professor)
+// , MAX(Singer)
+// , MAX(Actor)
+// FROM
+//     (SELECT CASE WHEN Occupation = 'Doctor' THEN name END AS Doctor
+//     , CASE WHEN Occupation = 'Professor' THEN name END AS Professor
+//     , CASE WHEN Occupation = 'Singer' THEN name END AS Singer
+//     , CASE WHEN Occupation = 'Actor' THEN name END AS Actor
+//     , RANK() OVER (PARTITION BY Occupation ORDER BY Name) AS occu_rank
+//     FROM Occupations) x
+// GROUP BY occu_rank
