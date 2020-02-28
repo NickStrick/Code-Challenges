c253d0a5120f220ab3d2fd84100f727a4a3aaa4d
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-02-27 19:27:15 -0600

    heightChecker solution

--- LeetCode/HeightChecker.py
+++ LeetCode/HeightChecker.py
@@ -1,1 +1,11 @@
 # https://leetcode.com/problems/height-checker/
+
+
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        sorts = sorted(heights)
+        count = 0
+        for x, y in zip(heights, sorts):
+            if x != y:
+                count += 1
+        return count
