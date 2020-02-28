ae66b82ba1b9e00848ad1f690aae91f5cc543250
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-02-27 19:29:26 -0600

    height checker js solution

--- LeetCode/HeightChecker.py
+++ LeetCode/HeightChecker.py
@@ -13,3 +13,25 @@ class Solution:
 
 heights = [1, 1, 4, 2, 1, 3]
 print(Solution.heightChecker(heights))
+
+
+# JS SOLUTION
+
+# heights = [1,1,4,2,1,3]
+# class Solution:
+#     def heightChecker(self, heights):
+#         sorts = sorted(heights)
+#         count = 0
+#         for x, y in zip(heights, sorts):
+#             if x != y:
+#                 count += 1
+#         return count
+# s = Solution()
+# print(s.heightChecker(heights))
+
+
+# class Solution:
+#     def heightChecker(self, heights: List[int]) -> int:
+#         ns = set([(key,value) for (key,value) in enumerate(heights)])
+#         s = set([(key,value) for (key,value) in enumerate(sorted(heights))])
+#         return len(heights) - len(ns & s)
