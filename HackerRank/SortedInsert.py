3f29360f254735770bb30411d2a789c605e4817b
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-02-28 17:55:48 -0600

    sortedinsert rust implementaion

--- HackerRank/SortedInsert.py
+++ HackerRank/SortedInsert.py
@@ -29,3 +29,36 @@ def sortedInsert(head, data):
         head.next = rest
         rest.prev = head
         return head
+
+
+# struct List<T> {
+# 	value: T,
+# 	next: Option<Box<List<T>>>
+# }
+# ​
+# impl<T> List<T> {
+# 	fn new(v: T) -> Self {
+# 		List { value: v, next: None }
+# 	}
+# }
+# ​
+# type ListNode<T> = Option<Box<List<T>>>;
+# ​
+# fn sorted_insert(l: ListNode<i32>, data: i32) -> ListNode<i32> {
+# 	match l {
+# 		Some(mut node) => {
+# 			// base case: we found the spot where the new node needs to go
+# 			if data <= node.value {
+# 				return List { value: data, next: node };
+
+# 			// otherwise, get the rest of the list by recursing
+# 			// set the rest of the list to be this node's next
+# 			} else {
+# 				let rest = sorted_insert(l.next, data);
+# 				node.next = rest;
+# 			}
+# 		},
+# 		// no head node, just return the new node
+# 		None => return List::new(data);
+# 	}
+# }
