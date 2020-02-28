c88c050359efea32d0c4af4071a3199a2ab3cb8f
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-02-28 17:55:03 -0600

    sortedinsert solution

--- HackerRank/SortedInsert.py
+++ HackerRank/SortedInsert.py
@@ -1,2 +1,31 @@
 # https://www.hackerrank.com/challenges/insert-a-node-into-a-sorted-doubly-linked-list/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=linked-lists
 # https://youtu.be/-1i95V-d7BY
+
+
+# DoublyLinkedListNode:
+#     int data
+#     DoublyLinkedListNode next
+#     DoublyLinkedListNode prev
+#
+#
+
+def sortedInsert(head, data):
+    # create our DLL node
+    new_node = DoublyLinkedListNode(data)
+    # what's the base case(s)?
+    # we also have to check if there is no head
+    if not head:
+        return new_node
+    if data < head.data:
+        # insert it before the head
+        new_node.next = head
+        head.prev = new_node
+        return new_node
+    # if we aren't in the base case, how do we get there?
+    else:
+        # there is a head and data > head.data
+        rest = sortedInsert(head.next, data)
+        # hook up the rest of the list with the head
+        head.next = rest
+        rest.prev = head
+        return head
