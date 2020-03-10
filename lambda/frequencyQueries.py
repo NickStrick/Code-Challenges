17d4cd73135313b271f45e7c11fd1aa419b4d547
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-03-09 20:06:43 -0500

    frequencyqueries rust

--- lambda/frequencyQueries.py
+++ lambda/frequencyQueries.py
@@ -49,3 +49,83 @@ def freqQuery(queries):
                 answers.append(0)
 
     return answers
+
+# JS IMPLEMENTATION
+# function frequencyQueries(queries) {
+#     const answers = [];
+#     // keeps track of the number of occurrences of eacy query value
+#     const occurrences = {};
+#     // keeps track of how many values have shown up a certain number of times
+#     // keys are integers representing frequency and values are the number
+#     // of values that showcase that frequency
+#     // for example, if a query specifies a new value, then that value has
+#     // only shown up once, so we'll increment the value associated with
+#     // the key of 1 to indicate that there is an additional value that
+#     // has shown up once
+#     const frequencies = {};
+# ​
+#     for (const [op, val] of queries) {
+#         if (op === 1) {
+#             // subtract an occurrence of the value's prior frequency
+#             frequencies[occurrences[val]]--;
+#             // add the value to our occurrences map
+#             occurrences[val] = (occurrences[val] || 0) + 1;
+#             // increment an occurrence of the value's new frequency
+#             frequencies[occurrences[val]] = (frequencies[occurrences[val]] || 0) + 1;
+#         } else if (op === 2 && occurrences[val]) {
+#             // subtract an occurrence of the value's prior frequency
+#             frequencies[occurrences[val]]--;
+#             // remove the value from our occurrences map
+#             occurrences[val]--;
+#             // increment an occurrence of the value's new frequency
+#             frequencies[occurrences[val]]++;
+#         } else if (op === 3) {
+#             // all we have to do for operation 3 is check if the value
+#             // associated with the frequency > 0
+#             answers.push(frequencies[val] > 0 ? 1 : 0);
+#         }
+#     }
+# ​
+#     return answers;
+# }
+
+
+# RUST IMPLEMENTATION
+
+# use std::collections::HashMap;
+# ​
+# fn frequency_queries(queries: Vec<(i32, i32)>) -> Vec<i32> {
+#   let mut val_counts: HashMap<i32, i32> = HashMap::new();
+#   let mut freq_counts: HashMap<i32, i32> = HashMap::new();
+#   let mut answers = vec![];
+# ​
+#   for (i, j) in queries {
+#     match i {
+#       1 => {
+#         let f = val_counts.entry(j).or_insert(0);
+#         // decrement j's value in freq_counts
+#         freq_counts.entry(*f).and_modify(|v| if *v > 0 { *v -= 1 }).or_insert(0);
+#         // increment j's value in val_counts
+#         *f += 1;
+#         // increment j's value in freq_counts
+#         freq_counts.entry(*f).and_modify(|v| *v += 1).or_insert(1);
+#       },
+#       2 => {
+#         let f = val_counts.entry(j).or_insert(0);
+#         // decrement j's value in freq_counts
+#         freq_counts.entry(*f).and_modify(|v| if *v > 0 { *v -= 1 }).or_insert(0);
+#         // decrement j's value in val_counts
+#         if *f > 0 { *f -= 1; }
+#         // increment j's value in freq_counts
+#         freq_counts.entry(*f).and_modify(|v| *v += 1).or_insert(1);
+#       },
+#       3 => {
+#         let fc = freq_counts.entry(j).or_insert(0);
+#         if *fc > 0 { answers.push(1); } else { answers.push(0); }
+#       },
+#       _ => panic!("Got an unexpected query number"),
+#     }
+#   }
+# ​
+#   answers
+# }
