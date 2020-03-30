72196a40ddcf0b3c43b5eb5efcd63f4135c55224
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-03-30 16:38:01 -0500

    flatenbinarytree solution

--- LeetCode/FlatenBinaryTree.py
+++ LeetCode/FlatenBinaryTree.py
@@ -1,1 +1,49 @@
 # https://leetcode.com/problems/flatten-binary-tree-to-linked-list/submissions/
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution:
+    def __init__(self):
+        self.resultArr = []
+
+    def flatten(self, root: TreeNode) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        # while numbers on the left
+        # take number left of root
+        # insert current number in right of tree
+
+        # while root has left node
+        # add left node to array
+        # insert array in right of root
+
+        self.addToStack(root.left)
+        print(self.resultArr)
+
+        currentNode = root
+        currentNode.left = None
+        nextNode = root.right
+        while len(self.resultArr) > 0:
+
+            newValue = self.resultArr.pop(0)
+            currentNode.right = TreeNode(newValue)
+
+            currentNode = currentNode.right
+
+        currentNode.right = nextNode
+
+    def addToStack(self, root):
+        # stack = self.stack
+        # resultArr= self.resultArr
+        if root:
+            self.resultArr.append(root.val)
+
+            self.addToStack(root.left)
+
+            self.addToStack(root.right)
