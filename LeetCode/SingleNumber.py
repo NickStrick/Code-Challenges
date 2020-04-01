2e3db8a5ea51c6a8df0b5e9d8939cffbecfe3032
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-04-01 11:02:03 -0500

    singlenumber solution

--- LeetCode/SingleNumber.py
+++ LeetCode/SingleNumber.py
@@ -1,1 +1,12 @@
 # https://leetcode.com/problems/single-number/
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+
+        numberSet = set()
+
+        for num in nums:
+            if num in numberSet:
+                numberSet.remove(num)
+            else:
+                numberSet.add(num)
+        return next(iter(numberSet))
