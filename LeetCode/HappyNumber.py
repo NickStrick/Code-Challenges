1ffe770aa27e92dd2a95e4c5b728a441552625d2
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-04-02 21:09:10 -0500

    happynumber cases and sum

--- LeetCode/HappyNumber.py
+++ LeetCode/HappyNumber.py
@@ -1,1 +1,16 @@
 # https://leetcode.com/explore/other/card/30-day-leetcoding-challenge/528/week-1/3284/
+
+
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        if n == 1:
+            return True
+
+        def SumDigits(num):
+
+            currSum = 0
+            while (num):
+                digit = num % 10
+                num = math.floor(num/10)
+                currSum += digit*digit
+            return currSum
