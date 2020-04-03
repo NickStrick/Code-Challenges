fb4adc30fb40e3be17002c216e940f9e9b07df50
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-04-02 21:07:33 -0500

    xor singlenumber solution

--- LeetCode/SingleNumber.py
+++ LeetCode/SingleNumber.py
@@ -10,3 +10,30 @@ class Solution:
             else:
                 numberSet.add(num)
         return next(iter(numberSet))
+
+
+# Hash table solutionclass Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+
+        hash_table = defaultdict(int)
+        for i in nums:
+            hash_table[i] += 1
+
+        for i in hash_table:
+            if hash_table[i] == 1:
+                return i
+
+# XOR bitwise solution
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+
+        # return 2 * sum(set(nums)) - sum(nums)
+
+        # https://data-flair.training/blogs/python-bitwise-operators/
+        a = 0
+        for i in nums:
+            a ^= i
+            print(a, i)
+        return a
