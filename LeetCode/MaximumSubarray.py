e33028cc0da8d4f064539235a33335205b1c7a45
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-04-03 21:27:38 -0500

    maxsubarray solution

--- LeetCode/MaximumSubarray.py
+++ LeetCode/MaximumSubarray.py
@@ -1,4 +1,17 @@
 # https://leetcode.com/explore/other/card/30-day-leetcoding-challenge/528/week-1/3285/
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        pass
+        if len(nums) == 1:
+            return nums[0]
+
+        max = nums[0]
+        currSum = 0
+
+        for num in nums:
+            currSum += num
+            if currSum > max:
+                max = currSum
+            if currSum < 0:
+                currSum = 0
+
+        return max
