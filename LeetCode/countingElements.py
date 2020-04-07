d450d5282367a79bd5b25ab51b3495a78958f785
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-04-07 16:41:17 -0500

    countingelements solution

--- LeetCode/countingElements.py
+++ LeetCode/countingElements.py
@@ -1,1 +1,13 @@
 # https://leetcode.com/explore/other/card/30-day-leetcoding-challenge/528/week-1/3289/
+
+
+class Solution:
+    def countElements(self, arr: List[int]) -> int:
+        arrSet = set(arr)
+        count = 0
+
+        for num in arr:
+            if num+1 in arrSet:
+                count += 1
+
+        return count
