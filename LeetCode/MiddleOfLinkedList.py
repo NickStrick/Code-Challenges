9c818bcf6bbf22a192f00508669bb5a18f3e45e0
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-04-08 21:17:31 -0500

    middlelinkedlist solution

--- LeetCode/MiddleOfLinkedList.py
+++ LeetCode/MiddleOfLinkedList.py
@@ -1,1 +1,23 @@
 # https://leetcode.com/explore/other/card/30-day-leetcoding-challenge/529/week-2/3290/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+class Solution:
+    def middleNode(self, head: ListNode) -> ListNode:
+
+        first = head
+        second = head.next
+
+        while first is not None and second is not None:
+            first = first.next
+            if second.next:
+                second = second.next.next
+            else:
+                second = None
+
+        return first
