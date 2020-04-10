c4f25080aed916fcc3b686d629099b0a00db1235
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-04-09 21:48:11 -0500

    backspacecompare  code

--- LeetCode/backspaceCompare.py
+++ LeetCode/backspaceCompare.py
@@ -1,2 +1,16 @@
 # https://leetcode.com/explore/other/card/30-day-leetcoding-challenge/529/week-2/3291/
 # https://repl.it/@Nstricker/PeacefulCraftyGenerics
+
+
+def backspaceCompare(S: str, T: str) -> bool:
+
+    # strObj = strObj[0 : 5 : ] + strObj[5 + 1 : :]
+
+    def backspace(string, i):
+        string = string[:i-1] + string[i+1:]
+        print(string)
+        return string
+
+    index = S.find('#')
+    S = backspace(S)
+    print(S.find('#'), S)
