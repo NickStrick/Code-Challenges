a50f670d9f31b7775972d171008db3cb6c18ad77
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-04-13 21:53:13 -0500

    conesecbinary solution

--- LeetCode/consecutiveBinary.py
+++ LeetCode/consecutiveBinary.py
@@ -1,2 +1,10 @@
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
+        i = 0
+        add = nums[0]
+        print(len(nums))
+
+        while i+add % 2 == nums[i] and i < len(nums) - 1:
+            print(i, i+add % 2,  nums[i])
+            i += 1
+        return i+1
