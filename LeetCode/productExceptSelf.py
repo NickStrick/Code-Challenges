b89d096d87fc3263c9fd8153e25c7817f07e8cf3
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-04-16 17:21:41 -0500

    productexceptself solution

--- LeetCode/productExceptSelf.py
+++ LeetCode/productExceptSelf.py
@@ -1,1 +1,12 @@
 # https://leetcode.com/explore/other/card/30-day-leetcoding-challenge/530/week-3/3300/
+def productExceptSelf(self, nums: List[int]) -> List[int]:
+    if len(nums) == 1:
+        return nums
+    res = []
+    for i in range(len(nums)):
+        currVal = 1
+        for x in nums:
+            if x != nums[i]:
+                currVal *= x
+        res.append(currVal)
+    return res
