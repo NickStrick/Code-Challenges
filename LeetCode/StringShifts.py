2135776e179433963ffdbf9ec2135970dc0c1cbf
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-04-15 21:13:57 -0500

    strinshifts solution

--- LeetCode/StringShifts.py
+++ LeetCode/StringShifts.py
@@ -1,1 +1,19 @@
 # https://leetcode.com/explore/other/card/30-day-leetcoding-challenge/529/week-2/3299/
+
+
+def stringShift(self, s: str, shift: List[List[int]]) -> str:
+
+    for command in shift:
+        for x in range(command[1]):
+            print(command[0])
+
+            if command[0] == 0:
+                i = s[0]
+                s = s[1:]
+                s += i
+            else:
+                i = s[len(s)-1]
+                s = s[:-1]
+                i += s
+                s = i
+    return s
