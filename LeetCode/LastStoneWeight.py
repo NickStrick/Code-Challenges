20837c22f5bef376ed10646f43eabda730feda5f
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-04-17 17:26:57 -0500

    laststone solution

--- LeetCode/LastStoneWeight.py
+++ LeetCode/LastStoneWeight.py
@@ -1,1 +1,18 @@
 # https://leetcode.com/explore/other/card/30-day-leetcoding-challenge/529/week-2/3297/
+
+
+def lastStoneWeight(self, stones: List[int]) -> int:
+
+    while len(stones) > 1:
+        stones.sort(reverse=True)
+
+        y = stones[0]
+        x = stones[1]
+        stones.remove(y)
+        stones.remove(x)
+
+        new = y-x
+        if new:
+            stones.append(new)
+
+    return stones[0]
