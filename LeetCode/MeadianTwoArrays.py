8caa95a062aac1aab5182e94426c712cc075beff
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-04-28 15:44:28 -0500

    meadiantwoarrays solution

--- LeetCode/MeadianTwoArrays.py
+++ LeetCode/MeadianTwoArrays.py
@@ -1,1 +1,13 @@
 # https://leetcode.com/problems/median-of-two-sorted-arrays/
+
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        nums1.extend(nums2)
+        nums1.sort()
+        print(nums1)
+        m1 = nums1[math.floor(len(nums1)/2)]
+        m2 = m1+1
+        if nums1
+        res = (nums1[m1]+nums1[m2])/2
+        return res
