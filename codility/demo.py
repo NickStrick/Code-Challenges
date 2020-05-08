95b2c6340e1d882f08b69fcfb4e87e96a9e7e8b9
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-05-08 17:57:02 -0500

    demo solution

--- codility/demo.py
+++ codility/demo.py
@@ -1,1 +1,14 @@
 # https://app.codility.com/demo/results/demoX6AERE-32T/
+
+
+def solution(A):
+    # write your code in Python 3.6
+    smallestInt = 1
+    A.sort()
+    for el in A:
+        if smallestInt == el:
+            smallestInt += 1
+        elif el > smallestInt:
+            return smallestInt
+
+    return smallestInt
