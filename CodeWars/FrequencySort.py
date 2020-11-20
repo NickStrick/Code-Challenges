552bc15c7e9f6fab8f77a7b1cf84fed6dfec441a
Nick Stricker <strickerdev@gmail.com>
2020-11-19 18:36:27 -0600

    FREQUENCYSORT SOLUTION

--- CodeWars/FrequencySort.py
+++ CodeWars/FrequencySort.py
@@ -1,1 +1,24 @@
 #https://www.codewars.com/kata/5a8d2bf60025e9163c0000bc/train/python
+
+def solve(arr):
+    # loop throug list put value and count in a dictionary
+    # return  new list with dictionary
+    arrCount = {}
+
+    for i in arr:
+        if i in arrCount:
+            arrCount[i] += 1
+        else: arrCount[i] = 1
+
+    arrCount = {k: v for k, v in sorted(arrCount.items(), key=lambda item: item[1])}
+    print(arrCount)
+    
+
+    result = []
+
+    for num in arrCount:
+        while arrCount[num] > 0:
+            arrCount[num] -= 1
+            result.append(num)
+    
+    return result
