0a33487ace81ae7a46524c17cb365e62bb321794
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-11-24 15:20:41 -0600

    threeentries description

--- dailyCodingProblem/threeEntries.js
+++ dailyCodingProblem/threeEntries.js
@@ -1,1 +1,15 @@
 //  https://repl.it/@Nstricker/problem339#index.js
+
+// Good morning! Here's your coding interview problem for today.
+
+// This problem was asked by Microsoft.
+
+// Given an array of numbers and a number k, determine if there are three entries in the array which add up to the specified number k. For example, given [20, 303, 3, 4, 25] and k = 49, return true as 20 + 4 + 25 = 49.
+
+
+//  loop through array
+// add current index to solution list
+// loop through rest of list after index, 
+
+// can we do this recursively?
+// 
