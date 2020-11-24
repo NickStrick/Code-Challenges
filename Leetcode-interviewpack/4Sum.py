34f439ec3c0f5f902ec0ea3d6726a26e113aca5e
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-11-23 20:05:31 -0600

    4sum class creation

--- Leetcode-interviewpack/4Sum.py
+++ Leetcode-interviewpack/4Sum.py
@@ -1,1 +1,10 @@
 # https://leetcode.com/explore/interview/card/top-interview-questions-medium/103/array-and-strings/776/
+
+
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        return
