cb728646003c07ee2627dbfb6cd1af477b34c04d
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-11-25 22:54:59 -0600

    maniplulate description

---
+++ RanktheHack/ManipulateArray.js
@@ -0,0 +1,6 @@
+//Complete the function arrayManipulation in the editor below. It must return an integer, the maximum value in the resulting array.
+// arrayManipulation has the following parameters:
+// n - the number of elements in your array
+// queries - a two dimensional array of queries where each queries[i] contains three integers, a, b, and k.
+
+// Complete the arrayManipulation function below.
