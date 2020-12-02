87fb7b66512c687363e90abea91e475cff41f9f5
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-12-01 18:43:04 -0600

    starting coded

--- RanktheHack/RotationLeft.js
+++ RanktheHack/RotationLeft.js
@@ -1,2 +1,26 @@
 // Left Rotation
 'use strict';
+rocess.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine(nums, d) {
+    let left = nums.splice(0, d)
+    nums = [...nums, ...left]
+    console.log(nums.join(' '));
+    return nums.join(' ')
+}
