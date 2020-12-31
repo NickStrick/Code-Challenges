bcce27855462bb84a121532fda69e150453bd7b0
Nick Stricker <strickerdev@gmail.com>
2020-12-31 16:50:44 -0600

    encryption solution

--- RanktheHack/Encryption.js
+++ RanktheHack/Encryption.js
@@ -1,3 +1,20 @@
 //https://www.hackerrank.com/challenges/encryption/problem
 
-function encryption(s) {}
+
+function encryption(s) {
+    let result = '';
+    let length = s.length;
+
+    let cols = Math.ceil(Math.sqrt(length));
+
+    for (let i = 0; i < cols; i++) {
+
+        for (let j = i; j < length; j += cols) {
+            result += s[j];
+        }
+        result += ' ';
+    }
+
+    return result;
+
+}
