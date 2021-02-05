e8e3955e36b4b5adf6e22386411af8b57c3da627
Nick Stricker <strickerdev@gmail.com>
2021-02-04 20:03:52 -0600

    pseudo code

--- dailyCodingProblem/google-interview.js
+++ dailyCodingProblem/google-interview.js
@@ -6,3 +6,9 @@
 
 // For example, given 'waterrfetawx' and a k of 2, you could delete f and x to get 'waterretaw'.
 
+//palindrome conditions:
+  //if is empty or all letters the same it is a palindrome
+  // then compare first and last of string, 
+  // if not same, not palindrome, remove, decrease k
+  // loop till k == 0
+  // retrun true or false
