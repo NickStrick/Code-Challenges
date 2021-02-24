8f9cd94ada263e4775011afdf8e2e4af1b1f5d61
Nick Stricker <strickerdev@gmail.com>
2021-02-23 18:09:34 -0600

    paraenthfcbk test

--- dailyCodingProblem/ParenthesesFacebook.js
+++ dailyCodingProblem/ParenthesesFacebook.js
@@ -3,8 +3,13 @@
 // Given a string of parentheses, find the balanced string that can be produced from it using 
 // the minimum number of insertions and deletions. If there are multiple solutions, return any of them.
 
+// insertoins and deletions both count. multiple solutins.
+
 // For example, given "(()", you could return "(())". Given "))()(", you could return "()()()()".
 
 function findParentheses(str){
-    
+
 }
+
+console.log(findParentheses("(()"))     //(())
+console.log(findParentheses("))()("))  //()()()()
