dbda822a64f648e3b5cf95eced191d29801f6a4e
Nick Stricker <strickerdev@gmail.com>
2021-03-16 21:12:18 -0500

    batterbox tests

--- dailyCodingProblem/BatterBox.js
+++ dailyCodingProblem/BatterBox.js
@@ -34,6 +34,23 @@
 //     /
 //    1
 
+// We do not remove the tree at the root or its left child because it still has a 1 as a descendant.
+//    0
+//   / \
+//  1   0
+//     / \
+//    1   0
+//   / \    \
+//  0   1    1
+// should be pruned to:
+//    0
+//   / \
+//  1   0
+//     / \
+//    1   0
+//     \    \
+//      1    1
+
 function pruneProblem(headNode){
     // if node has no children, check if 1 or 0, if 0 delete node
     // if children, call function on children
@@ -95,7 +112,17 @@ rootOne.right.left.left = new Node(0);
 rootOne.right.right = new Node(0);
 rootOne.right.right.right = new Node(0);
 
+let rootTwo = new Node(0);
+rootTwo.left = new Node(1);
+rootTwo.right = new Node(0);
+rootTwo.right.left = new Node(1);
+rootTwo.right.left.right = new Node(1);
+rootTwo.right.left.left = new Node(0);
+rootOne.right.right.right = new Node(1);
 
+
+rootTwo.print()
+pruneProblem(rootTwo).print()
 rootOne.print()
 pruneProblem(rootOne).print()
 // console.log(pruneProblem());
