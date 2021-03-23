854426509c76c6201d7b4f2dfdc922eec1c261a9
Nick Stricker <strickerdev@gmail.com>
2021-03-22 19:55:08 -0500

    anagram search functiuon"

--- dailyCodingProblem/anagramSearch.js
+++ dailyCodingProblem/anagramSearch.js
@@ -7,3 +7,7 @@
 //  Given a word W and a string S, find all starting indices in S which are anagrams of W.
  
 //  For example, given that W is "ab", and S is "abxaba", return 0, 3, and 4.
+
+function anagramSearch(W, S){
+
+}
