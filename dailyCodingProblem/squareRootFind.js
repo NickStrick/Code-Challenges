1b58796e3558076826d5445d588453faa175e7e8
Nick Stricker <strickerdev@gmail.com>
2021-04-01 20:15:49 -0500

    squarerootfine first solution

--- dailyCodingProblem/squareRootFind.js
+++ dailyCodingProblem/squareRootFind.js
@@ -1,1 +1,13 @@
 // Given a real number n, find the square root of n. For example, given n = 9, return 3.
+
+function squareRoot(n){
+
+    let i = 0;
+    let rooted = i * i;
+
+    while( rooted != n && i < 50){
+        i++
+        rooted = i*i;
+    }
+    return i;
+}
