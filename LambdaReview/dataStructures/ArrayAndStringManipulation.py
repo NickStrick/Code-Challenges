1f1d5a532f3dfe34163582bd48f21a6d2548b72f
Nick Stricker <strickerdev@gmail.com>
2021-04-02 18:58:06 -0500

    review orginization

--- LambdaReview/dataStructures/ArrayAndStringManipulation.py
+++ LambdaReview/dataStructures/ArrayAndStringManipulation.py
@@ -5,8 +5,8 @@
 def in_place_reverse(arr):
     # have two pointers one at one end the other at length and replace and reverse list until pointers are the same index
 
-    left = 0;
-    right = len(arr) - 1;
+    left = 0
+    right = len(arr) - 1
 
     while left < right:
         # swap the values at these two pointers
@@ -19,4 +19,4 @@ def in_place_reverse(arr):
     return arr
 
 arr = [1,2,3,4,5,6,7,8]
-print(in_place_reverse(arr));
+print(in_place_reverse(arr))
