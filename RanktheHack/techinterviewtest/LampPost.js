18db5b9c544fe2179a0bb442539b703535ed6173
Nick Stricker <strickerdev@gmail.com>
2021-04-08 18:50:01 -0500

    lampPost problem, lambda fellows test

---
+++ RanktheHack/techinterviewtest/LampPost.js
@@ -0,0 +1,13 @@
+// you are given an arrray of objects, where the value is the index of the object
+// you are given a lamppost radius
+
+// determine the maximum number of objects the lamp can have lit, if multiple use earliest index
+
+// objects, radius
+// [-5, 3, 4, 9], 5
+// return -1
+
+//              *
+//              |
+//  =           |       = =         =
+// -5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9
