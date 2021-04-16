e3034a7475be22f08e0f28e3f5297b3908674a67
Nick Stricker <strickerdev@gmail.com>
2021-04-15 21:16:32 -0500

    falling cubes understanding/Plan layout

--- RanktheHack/techinterviewtest/fallingCubes.js
+++ RanktheHack/techinterviewtest/fallingCubes.js
@@ -9,6 +9,39 @@ return the number of bricks it takes to make the connected blocks touch the bott
 let matrix = [  ['*', '*', '*'],
                 ['#', '*', '*'],
                 ['.', '*', '#'],
-                ['#', '.', '.']] // returns 2 (top left and bottom right bricks only need to be removed)
+                ['#', '.', '.']] // returns 2 (top left and bottom right bricks only need to be removed for block to touch bottom)
+
+*/
+
+/*
+    i bassically have to move the solid blocks down, and remove bricks until, one of the solids touches the bottom of the matrix
+    - so find a brick, remove it, if solid can move down only replacing free spaces, move, else, find and next brick and check again
+        - do this until one of the solids reaches the final array in the matrix
+
+
+TEST INPUTS - step by step
+        - remove brick
+        - add to counter
+        - move solids if you can
+    - until solids in bottom array
+
+            ['*', '*', '*'],
+            ['.', '*', '*'],
+            ['.', '*', '#'],
+            ['#', '.', '.']
+            1
+
+            ['*', '*', '*'],
+            ['.', '*', '*'],
+            ['.', '*', '.'],
+            ['#', '.', '.']
+            2
+
+            ['', '', ''],
+            ['*', '*', '*'],
+            ['.', '*', '*'],
+            ['#', '*', '.']
+
+            return 2
 
 */
