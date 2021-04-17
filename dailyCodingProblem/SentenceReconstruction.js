2fa32a40727a973675cef92040b439428384ca37
Nick Stricker <strickerdev@gmail.com>
2021-04-16 21:43:22 -0500

    sentence reconstruction pesudo code

--- dailyCodingProblem/SentenceReconstruction.js
+++ dailyCodingProblem/SentenceReconstruction.js
@@ -8,14 +8,20 @@
 // Given the set of words 'bed', 'bath', 'bedbath', 'and', 'beyond', and the string "bedbathandbeyond", return either ['bed', 'bath', 'and', 'beyond] or ['bedbath', 'and', 'beyond'].
 
 function sentenceReconstruction(dictList, s){
+    /*
+        loop throught the array
+        each time check if current string in dictionary
+            if it is, restart string and add to result array
+
+    */
 
 }
-let diction = ['quick', 'brown', 'the', 'fox']
+let diction = {'quick':0, 'brown':1, 'the':2, 'fox':3}
 let sentence = "thequickbrownfox"
 
 console.log(sentenceReconstruction(diction, sentence)) // return ['the', 'quick', 'brown', 'fox']
 
-diction = ['bed', 'bath', 'bedbath', 'and', 'beyond'];
+diction = {'bed':0, 'bath':1, 'bedbath':2, 'and':3, 'beyond':4};
 sentence = "bedbathandbeyond"
 
 console.log(sentenceReconstruction(diction, sentence)) // return either ['bed', 'bath', 'and', 'beyond] or ['bedbath', 'and', 'beyond']
