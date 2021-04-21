35f3bac86682ebfd93a396caa329a8ce51c54412
Nick Stricker <strickerdev@gmail.com>
2021-04-21 18:44:37 -0500

    distinctLongestSubstring understanding

--- dailyCodingProblem/distinctLongestSubstring.js
+++ dailyCodingProblem/distinctLongestSubstring.js
@@ -4,3 +4,12 @@
 // find the length of the longest substring that contains at most k distinct characters.
 
 // For example, given s = "abcba" and k = 2, the longest substring with k distinct characters is "bcb".
+
+function distinctLongestSubstring(s, k){
+    // understand
+        // so basically at each index, count longest substring with k charaters
+            // if longer then previous longest, set as longest
+        // until character count is greater then k
+
+        // return longest
+}
