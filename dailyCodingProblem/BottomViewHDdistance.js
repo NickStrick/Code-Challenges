e4824069e59a12c764d19473a1d2ded98ceb1355
Nick Stricker <strickerdev@gmail.com>
2021-04-26 21:31:42 -0500

    node class and tests

--- dailyCodingProblem/BottomViewHDdistance.js
+++ dailyCodingProblem/BottomViewHDdistance.js
@@ -31,6 +31,23 @@ function hdBottomView(root){
 
 }
 
-let root = {}
+class Node{
+    constructor(value){
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+let root = new Node(5);
+root.left = new Node(3);
+root.left.left = new Node(1);
+root.left.right = new Node(4)
+root.left.left.left = new Node(0)
+//right
+root.right = new Node(7)
+root.right.left = new Node(6);
+root.right.right = new Node(9)
+root.right.right.left = new Node(8)
 
 console.log(hdBottomView(root))
