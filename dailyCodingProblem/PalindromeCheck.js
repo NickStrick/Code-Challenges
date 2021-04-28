6316b657a9e2ca618b2bf8e69b27664c56e5262d
Nick Stricker <strickerdev@gmail.com>
2021-04-27 23:05:24 -0500

    checkPalindrome plan pesudo

--- dailyCodingProblem/PalindromeCheck.js
+++ dailyCodingProblem/PalindromeCheck.js
@@ -4,10 +4,19 @@
 // For example, 121 is a palindrome, as well as 888. 678 is not a palindrome. 
 // Do not convert the integer into a string.
 
-function findPal(){
+function checkPal(){
+    // plan
+    // determine how to find the palindromes
+    // brute:
+        // go from the bottom up until your at or past number
+        
+    // non brute: 
+        // find a way to get it from above down
+        // use a math equsation to find this
+    
 
 }
 
-console.log(findPal(121)) // true
-console.log(findPal(888)) // true
-console.log(findPal(678)) // false
+console.log(checkPal(121)) // true
+console.log(checkPal(888)) // true
+console.log(checkPal(678)) // false
