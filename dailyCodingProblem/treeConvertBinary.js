4e8d465461ade87cfe33f409f4a961708b746532
Nick Stricker <strickerdev@gmail.com>
2021-05-03 19:57:06 -0500

    covertbinarytree tests

--- dailyCodingProblem/treeConvertBinary.js
+++ dailyCodingProblem/treeConvertBinary.js
@@ -35,4 +35,14 @@ class Node{
     }
 }
 
-console.log(covertTree())
+let root = new Node(0)
+root.right = new Node(2);
+root.right.right = new Node(4)
+root.right.right.left = new Node(6)
+root.right.right.right = new Node(7)
+//left
+root.left = new Node(1)
+root.left.left = new Node(3)
+root.left.left.right = new Node(5)
+
+console.log(covertTree(root))
