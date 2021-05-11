757519bd0369a9a6f1b1b19c70a36e925d427b61
Nick Stricker <strickerdev@gmail.com>
2021-05-11 18:20:20 -0500

    arrayRotate pesudo plan

--- dailyCodingProblem/arrayRotate.js
+++ dailyCodingProblem/arrayRotate.js
@@ -5,6 +5,10 @@
 
 function arrayRotate(arr){
 
+    //plan
+    //  take an O(1) k in array, split with array method
+    //  join  end + start portions of the array
+
 }
 
 console.log(arrayRotate([1,2,3,4,5,6,7,8], 4))
