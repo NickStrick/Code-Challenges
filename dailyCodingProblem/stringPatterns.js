8caa4a24602efbaaae7ad3668ae52bac4f051aba
Nick Stricker <strickerdev@gmail.com>
2021-05-28 16:54:43 -0500

    stringPattern function

--- dailyCodingProblem/stringPatterns.js
+++ dailyCodingProblem/stringPatterns.js
@@ -4,3 +4,7 @@
 // find the starting indices of all occurrences of the pattern in the string.
 //  For example, given the string "abracadabra" and the pattern "abr", 
 //  you should return [0, 7].
+
+function stringPattern(st, pat){
+    
+}
