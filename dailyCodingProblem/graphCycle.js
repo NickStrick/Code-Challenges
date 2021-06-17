6121c245259ebadd72ba692f48df51bc2bb1743b
Nick Stricker <strickerdev@gmail.com>
2021-06-17 17:29:33 -0500

    graphcycle pesudo plan

--- dailyCodingProblem/graphCycle.js
+++ dailyCodingProblem/graphCycle.js
@@ -4,8 +4,12 @@
 // determine if it contains a cycle.
 
 function graphCycle(graph){
+    // plan
+    // traverse graph in a way 
+    // that allows ability to determine when it has looped
+    // this can be done recursively
 
 }
 let myGraph = {}
 
-console.log(graphCycle(myGraph))
+console.log(graphCycle(myGraph));
